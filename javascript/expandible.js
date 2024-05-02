98f3f2244f3a08940e3dcde3f56475736aa4b8db
softimportaciones <168755116+softimportaciones@users.noreply.github.com>
2024-05-02 10:31:30 -0500

    Add files via upload

---
+++ javascript/expandible.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let toggles = document.querySelectorAll('.toggle h2');
+    
+
+    toggles.forEach(function(toggle) {
+        toggle.addEventListener('click', function() {
+            var listItem = toggle.parentElement.parentElement;
+            var text = listItem.querySelector('.texto_expandir');
+            text.style.display = text.style.display === 'block' ? 'none' : 'block'; // Alternar visibilidad
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let toggles = document.querySelectorAll('.toggle h1');
+    
+
+    toggles.forEach(function(toggle) {
+        toggle.addEventListener('click', function() {
+            var listItem = toggle.parentElement.parentElement;
+            var text = listItem.querySelector('.texto_expandir');
+            text.style.display = text.style.display === 'block' ? 'none' : 'block'; // Alternar visibilidad
+        });
+    });
+});
+
+
