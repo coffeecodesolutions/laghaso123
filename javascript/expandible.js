4e1f3e894b0e12434b58f7071cb77be97af348f4
coffeecodesolutions <168755116+coffeecodesolutions@users.noreply.github.com>
2024-05-03 00:21:00 -0500

    Add files via upload

--- javascript/expandible.js
+++ javascript/expandible.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     toggles.forEach(function(toggle) {
         toggle.addEventListener('click', function() {
             var listItem = toggle.parentElement.parentElement;
+            console.log(toggles);
             var text = listItem.querySelector('.texto_expandir');
             text.style.display = text.style.display === 'block' ? 'none' : 'block'; // Alternar visibilidad
         });
@@ -19,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
     toggles.forEach(function(toggle) {
         toggle.addEventListener('click', function() {
             var listItem = toggle.parentElement.parentElement;
+            console.log(listItem);
             var text = listItem.querySelector('.texto_expandir');
             text.style.display = text.style.display === 'block' ? 'none' : 'block'; // Alternar visibilidad
         });
